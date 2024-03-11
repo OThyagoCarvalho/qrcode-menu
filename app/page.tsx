e1610ac359a195b394f4c95a92d426d774ab25af
marise-san <marisemrtns@gmail.com>
2024-03-11 20:22:49 -0300

    feat: adicionando link na home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 'use client'
 
-import { Button } from "@nextui-org/react"
-import Link from "next/link"
-import { signOut, onAuthStateChanged } from 'firebase/auth';
-import { useEffect, useState } from "react"
+import { Button } from "@nextui-org/react";
+import { onAuthStateChanged, signOut } from 'firebase/auth';
+import Link from "next/link";
+import { useEffect, useState } from "react";
 import { AppUser } from "./interfaces/menu";
 import { auth } from "./lib/firebase";
 
 export default function Home() {
 
   const [user, setUser] = useState<AppUser>(undefined as unknown as AppUser);
-  
+
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
       if (firebaseUser) {
@@ -33,55 +33,59 @@ export default function Home() {
     <main className="flex min-h-screen items-center justify-center p-24">
       <div
         className='flex flex-nowrap p-24 bg-white rounded-3xl flex-col'
+      >
+        <h1
+          className="text-red-900 font-extrabold text-[72px]"
         >
-          <h1
-            className="text-red-900 font-extrabold text-[72px]"
-          > 
-            QR CODE MENU
-          </h1>
-          <ul
-            className="flex gap-4 justify-center items-center"
-          >
-           {
-            !user && 
-           <>
-           <li>
-              <Button className="bg-red-900 text-white">
-              <Link href={"sign-in"}>
-                  Entrar
-              </Link>
-              </Button>
-            </li>
-            <li>
-              <Button className="bg-red-900 text-white">
-              <Link href={"sign-up"}>
-                  Cadastrar
-              </Link>
-              </Button>
-            </li>
-           </> 
-            }
-            {
-              user &&
-              <>
-                <li>
+          QR CODE MENU
+        </h1>
+        <ul
+          className="flex gap-4 justify-center items-center"
+        >
+          {
+            !user &&
+            <>
+              <li>
                 <Button className="bg-red-900 text-white">
-                <Link href={"/dashboard"}>
+                  <Link href={"sign-in"}>
+                    Entrar
+                  </Link>
+                </Button>
+              </li>
+              <li>
+                <Button className="bg-red-900 text-white">
+                  <Link href={"sign-up"}>
+                    Cadastrar
+                  </Link>
+                </Button>
+              </li>
+            </>
+          }
+          {
+            user &&
+            <>
+              <li>
+                <Button className="bg-red-900 text-white">
+                  <Link href={"/dashboard"}>
                     Dashboard
-                </Link>
+                  </Link>
                 </Button>
-                </li>
-                <li>
-                <Button className="outline-1 text-white" onClick={()=> signOut(auth)}>
-                <Link href={"#"}>
+              </li>
+              <li>
+                <Button className="outline-1 text-white" onClick={() => signOut(auth)}>
+                  <Link href={"#"}>
                     Encerrar -&gt;
-                </Link>
+                  </Link>
                 </Button>
-                </li>
-              </>
-            }
-          </ul>
-      </div>     
+              </li>
+            </>
+          }
+        </ul>
+        <br />
+        <iframe width="560" height="315"
+          src="https://www.youtube.com/embed/2AD65qwmuCE?mute=1&autoplay=1"
+          title="Projeto Integrador"/>
+      </div>
     </main>
   )
 }
